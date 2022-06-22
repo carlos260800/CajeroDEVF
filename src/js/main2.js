64d55f658f2af93578b037056cb3cd6f30596ae2
carlos260800 <carlos260800@gmail.com>
2022-06-22 14:50:46 -0500

    primer commit

---
+++ src/js/main2.js
@@ -0,0 +1,64 @@
+var cuentas = [
+  { nombre: " Mali", saldo: 200 },
+  { nombre: "Gera", saldo: 290 },
+  { nombre: "Maui", saldo: 67 },
+];
+
+let ingresar_saldo = document.getElementById("ingresar_saldo");
+let retirar_saldo = document.getElementById("retirar_saldo");
+let input = document.getElementById("input");
+let input2 = document.getElementById("input2");
+let msg = document.getElementById("msg");
+let msg2 = document.getElementById("msg2");
+let nombre = document.getElementById("nombre");
+let saldo = document.getElementById("saldo");
+
+let cuenta = cuentas[1].saldo;
+let usuario = cuentas[1].nombre;
+
+mostrarNombre();
+mostrarSaldo();
+
+ingresar_saldo.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  agregarSaldo();
+});
+
+retirar_saldo.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  retirarSaldo();
+});
+
+const agregarSaldo = (evento) => {
+  if (input.value <= 0) {
+    msg.innerHTML = "<h3>Cantidad Invalida</h3>";
+  } else {
+    cuenta = cuenta + parseInt(input.value);
+    msg.innerHTML = `<h3>Se ingresaron ${input.value}$</h3>`;
+
+    mostrarSaldo();
+  }
+};
+
+const retirarSaldo = (evento) => {
+  if (input2.value <= 0 || input2.value > cuenta) {
+    msg2.innerHTML = "<h3>No puedes retirar esa cantidad</h3>";
+  } else {
+    cuenta = cuenta - parseInt(input2.value);
+    msg2.innerHTML = `<h3>Se retiraron ${input2.value}$</h3>`;
+    mostrarSaldo();
+  }
+};
+
+function mostrarNombre() {
+  nombre.innerHTML = `
+      <h3>${usuario}</h3>
+    `;
+}
+
+function mostrarSaldo() {
+  saldo.outerHTML;
+  saldo.innerHTML = `
+        <h3>${cuenta}</h3
+    `;
+}
